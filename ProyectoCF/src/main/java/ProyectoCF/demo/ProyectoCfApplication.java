b24f3672c01d0d88f79548dc0e653b4171f1ad44
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-02-14 00:45:48 -0600

    springvacio
    springvacio

---
+++ ProyectoCF/src/main/java/ProyectoCF/demo/ProyectoCfApplication.java
@@ -0,0 +1,13 @@
+package ProyectoCF.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCfApplication.class, args);
+	}
+
+}
---
+++ ProyectoCF/src/test/java/ProyectoCF/demo/ProyectoCfApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoCF.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
